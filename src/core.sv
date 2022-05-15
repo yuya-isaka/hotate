0791772670d9d461c7787cd2e42c018b2aeb6bde
Yuya Isaka <zxwg6329@gmail.com>
2022-05-15 23:54:00 +0900

    fix core comment

--- src/core.sv
+++ src/core.sv
@@ -13,9 +13,9 @@ module core (
   // パラメータ ---------------------------------------------------------------------
 
   parameter INST_MEM_SIZE = 32'd64;
-  parameter INST_ADDR_SIZE = 32'd7;
+  parameter INST_ADDR_SIZE = 32'd7; // -2してもおっけい？
   parameter DATA_MEM_SIZE = 32'd4096;
-  parameter DATA_ADDR_SIZE = 32'd13;
+  parameter DATA_ADDR_SIZE = 32'd13; // -2してもおっけい？
 
   // PC (WBで更新) ---------------------------------------------------------------------------
 
